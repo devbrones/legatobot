95d6c08739d290eea566c8526885ea34cbf9b444
devbrones <jonas.cron.gud@gmail.com>
2021-11-11 15:20:38 +0100

    Added comptime draft.

---
+++ drafts/comptime.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string>
+#include<chronos>
+#include<iostream>
+#include <chrono>  // chrono::system_clock
+#include <ctime>   // localtime
+#include <sstream> // stringstream
+#include <iomanip> // put_time
+#include <string>  // string
+using std::string;
+using std::cout;
+using std::endl;
+
+string rtime()
+{
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %H:%M:%S+00:00");
+    return ss.str();
+}
+
+
+string comptime(std::string aux) {
+	try{
+		string csvfile = aux.append(".csv");
+		std::fstream csvF(csvfile);
+		string line = getline(csvF,1);
+		int len = line.length();
+		line.erase(25, len);
+
+	}
+	catch (const std::exception& e) {
+		cout << "E[expt] - " << e.what() << endl;
+	}
+}
+
+
