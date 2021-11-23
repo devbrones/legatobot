6d3753cb54451b225247d2d17bd09ff1bcc04bf6
devbrones <jonas.cron.gud@gmail.com>
2021-11-23 09:55:07 +0000

    yes

--- cogs/cce_command.py
+++ cogs/cce_command.py
@@ -15,9 +15,11 @@ class Commands(commands.Cog):
     @commands.command(aliases=['add'])
     async def cce(self, ctx):
         a_ = csvFormater()
-        a_.csvConvert(nextup('hanna'))        
+        a_.csvConvert(nextup('hanna'))
+        print(a_.start)
         ar = vartoarray(a_.subject,a_.start,a_.finish,a_.classroom,a_.teachers,a_.agenda)
         createEvent(ctx.message.guild.id, ar)
+        print(ar)
 
         await ctx.send("Added a **server event** for class")
 
